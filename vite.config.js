d470cb44004e99d6b960a456506de492c44414c0
Tarek(Tanvir Hasan) <tarekhossain0043@gmail.com>
2025-09-30 01:17:08 +0600

    chore:vite configration update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+const repoName = 'todo-list-app';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:`/${repoName}`
 })
