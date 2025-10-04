faf430a034def8a6ddcced115b12668eeeb5c1ef
Tarek(Tanvir Hasan) <tarekhossain0043@gmail.com>
2025-10-04 18:44:50 +0600

    firebase database cutom id,edit,create,update and delete option added

--- src/functions/crud_or_helper.js
+++ src/functions/crud_or_helper.js
@@ -1,14 +1,40 @@
-const getRandomId = () => {
+import { getDoc, setDoc } from "firebase/firestore";
+import { db } from "../firebase/Fire";
+const getRadomId = () => {
   const characters =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  let randomID = "";
-  for (let i = 0; i < 16; i++) {
+  let randomId = "";
+  for (let i = 0; i < 15; i++) {
     const randomIndex = Math.floor(Math.random() * characters.length);
-    randomID += characters.charAt(randomIndex);
+    randomId += characters.charAt(randomIndex);
   }
-  return randomID;
+  return randomId;
 };
 
-export const createData = async (collectionName, data) => {
-  const id = getRandomId();
+export const createData = async (collectionData, data) => {
+  const id = getRadomId();
+  try {
+    const docRef = doc(db, collectionData, id);
+    await setDoc(docRef, {
+      id,
+      ...data,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const readData = async (collection, id) => {
+  try {
+    const docRef = doc(db, collection, id);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      console.log(docSnap.data());
+      return docSnap.data();
+    } else {
+      console.log("No much document!");
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
