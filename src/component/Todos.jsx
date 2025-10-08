16a458bbc667399c299da3808f0c0aa6d7ee637d
Tarek(Tanvir Hasan) <tarekhossain0043@gmail.com>
2025-10-09 01:17:36 +0600

    style:responsive issue solve

--- src/component/Todos.jsx
+++ src/component/Todos.jsx
@@ -76,91 +76,94 @@ export default function Todos() {
             {message}
           </p>
         ))}
-
-      <h2 className="text-center text-red-500 capitalize">
-        Hello {user.names}
-      </h2>
-      <button
-        onClick={changeNameFun}
-        className="px-4 py-3 bg-gray-200 text-red-400 capitalize transition-all ease-linear hover:text-green-300"
-      >
-        Change Name
-      </button>
-      <div className="add-new-todo mt-20 p-5 max-w-[400px] ml-[30px] h-full bg-gray-200 shadow-md shadow-red-200 rounded-md">
-        {/* <form action="#"> */}
-        <div className="input-field">
-          <input
-            type="text"
-            name="text"
-            value={newTodo}
-            onChange={(e) => setNewTodo(e.target.value)}
-            className="px-3 py-2 w-full bg-white outline-none rounded-sm text-[20px] capitalize font-normal"
-            placeholder="Add New Todos.."
-          />
-        </div>
-        <button
-          type="submit"
-          onClick={handleAddTodo}
-          className="px-6 inline-block mt-2 cursor-pointer py-3 bg-white shadow-sm shadow-gray-500 rounded-sm transition-colors ease-in-out hover:text-gray-500 capitalize font-semibold"
-        >
-          Add New
-        </button>
-        {/* </form> */}
-      </div>
-      <div className="w-[700px] h-full absolute top-1/2 right-4 -translate-y-1/2">
-        <h2 className="text-[40px] uppercase font-semibold text-center text-red-500">
-          my todo
+      <div className="container max-w-6xl m-auto">
+        <h2 className="text-center text-red-500 capitalize">
+          Hello {user.names}
         </h2>
-        {/* <form action="#"> */}
-        <input
-          type="text"
-          name="text"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          className="mt-2 px-3 py-2 w-full bg-white outline-none rounded-sm text-[20px] font-normal border-b border-b-gray-200 mb-3"
-          placeholder="Search Todos.."
-        />
-        {/* </form> */}
+        <button
+          onClick={changeNameFun}
+          className="px-4 py-3 bg-gray-200 text-red-400 capitalize transition-all ease-linear hover:text-green-300"
+        >
+          Change Name
+        </button>
+        <div className="grid gap-[80px] md:grid-cols-2">
+          <div className="add-new-todo flex flex-col items-center justify-center md:max-w-[400px]  mt-20 p-[40px] w-full ml-[30px] bg-gray-100 shadow-md shadow-[#ddd] rounded-md">
+            {/* <form action="#"> */}
+            <div className="input-field w-full">
+              <input
+                type="text"
+                name="text"
+                value={newTodo}
+                onChange={(e) => setNewTodo(e.target.value)}
+                className="px-3 py-2 w-full bg-white outline-none rounded-sm text-[20px] capitalize font-normal"
+                placeholder="Add New Todos.."
+              />
+            </div>
+            <button
+              type="submit"
+              onClick={handleAddTodo}
+              className="px-6 inline-block mt-2 cursor-pointer py-3 bg-white shadow-sm shadow-gray-500 rounded-sm transition-colors ease-in-out hover:text-gray-500 capitalize font-semibold"
+            >
+              Add New
+            </button>
+            {/* </form> */}
+          </div>
+          <div className="h-full">
+            <h2 className="text-[40px] uppercase font-semibold text-center text-red-500">
+              my todo
+            </h2>
+            {/* <form action="#"> */}
+            <input
+              type="text"
+              name="text"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              className="mt-2 px-3 py-2 w-full bg-white outline-none rounded-sm text-[20px] font-normal border-b border-b-gray-200 mb-3"
+              placeholder="Search Todos.."
+            />
+            {/* </form> */}
 
-        <table className="table m-auto text-center w-[600px] w-full z-[1] max-h-[500px] bg-white shadow-sm shadow-red-400">
-          <thead className="border table-caption">
-            <tr className="flex items-center justify-between p-3">
-              <th>#</th>
-              <th>Task</th>
-              <th>Completed</th>
-              <th>Edit/Delete</th>
-            </tr>
-          </thead>
-          <tbody className="table-caption border py-1 px-4">
-            {visibleTodos.length === 0 ? (
-              <p>No Todos Yeat.</p>
-            ) : (
-              visibleTodos.map((todo) => (
-                <tr
-                  key={todo.id}
-                  className="py-2 border-b flex items-center justify-between last:border-b-0"
-                >
-                  <td>{todo.id}</td>
-                  <td>{todo.task}</td>
-                  <td>{todo.completed}</td>
-                  <td>
-                    <button className="px-4 py-3 hover:bg-gray-500 bg-gray-200 text-green-300 transition-colors ease-in-out">
-                      Edit
-                    </button>
-                  </td>
-                  <td>
-                    <button
-                      onClick={() => handleDeleteTodo(todo.id)}
-                      className="px-4 py-3 hover:bg-gray-500 bg-gray-200 text-red-300 transition-colors ease-in-out"
-                    >
-                      Delete
-                    </button>
-                  </td>
+            <table className="table text-center w-full z-[1] max-h-[500px] bg-white shadow-sm shadow-red-400">
+              <thead className="border table-caption">
+                <tr className="flex items-center justify-between p-3">
+                  <th>#</th>
+                  <th>Task</th>
+                  <th>Completed</th>
+                  <th>Edit/Delete</th>
                 </tr>
-              ))
-            )}
-          </tbody>
-        </table>
+              </thead>
+              <tbody className="table-caption border py-1 px-4">
+                {visibleTodos.length === 0 ? (
+                  <p>No Todos Yeat.</p>
+                ) : (
+                  visibleTodos.map((todo) => (
+                    <tr
+                      key={todo.id}
+                      className="py-2 border-b flex items-center justify-between last:border-b-0"
+                    >
+                      <td>{todo.id}</td>
+                      <td>{todo.task}</td>
+                      <td>{todo.completed}</td>
+                      <td>
+                        <button className="px-4 py-3 hover:bg-gray-500 bg-gray-200 text-green-300 transition-colors ease-in-out">
+                          Edit
+                        </button>
+                      </td>
+                      <td>
+                        <button
+                          onClick={() => handleDeleteTodo(todo.id)}
+                          className="px-4 py-3 hover:bg-gray-500 bg-gray-200 text-red-300 transition-colors ease-in-out"
+                        >
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  ))
+                )}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   );
